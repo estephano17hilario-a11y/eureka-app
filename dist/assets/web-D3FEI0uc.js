ff9b38accbe232bf4a7fc12c03f1b510bfa618b8
Matrix4012Fpro <estephano17hilario@gmail.com>
2026-08-27 12:11:18 -0500

    feat: diseño fiel a la imagen 2 con capas de cartas apiladas debajo limpias y elegantes para oclusiones e invertidos

--- dist/assets/web-D3FEI0uc.js
+++
@@ -1,2 +0,0 @@
-import{W as n}from"./index-DItV8bZ-.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
-//# sourceMappingURL=web-D3FEI0uc.js.map
---
+++ dist/assets/web-DoZHVt8m.js
@@ -0,0 +1,2 @@
+import{W as n}from"./index-CSuML-Xu.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
+//# sourceMappingURL=web-DoZHVt8m.js.map
